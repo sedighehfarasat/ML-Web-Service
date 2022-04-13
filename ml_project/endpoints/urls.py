6f7878e6005fad762e53e776270d5c671aa87ca4
Sedigheh Farasat <sd.farasat@gmail.com>
2022-04-13 12:20:44 +0430

    Added ml

--- ml_project/endpoints/urls.py
+++ ml_project/endpoints/urls.py
@@ -1,4 +1,4 @@
-from django.urls import re_path, include
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import EndpointViewSet
 from .views import MLAlgorithmViewSet
@@ -7,12 +7,12 @@ from .views import MLRequestViewSet
 
 
 router = DefaultRouter(trailing_slash=False)
-router.register(r"endpoints", EndpointViewSet, basename="endpoints")
-router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
-router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
-router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
+router.register("endpoints", EndpointViewSet, basename="endpoints")
+router.register("mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register("mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register("mlrequests", MLRequestViewSet, basename="mlrequests")
 
 
 urlpatterns = [
-    re_path(r"^api/v1/", include(router.urls)),
+    path("api/v1/", include(router.urls)),
 ]
--- ml_project/ml_project/settings.py
+++ ml_project/ml_project/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'endpoints.apps.EndpointsConfig',
     'rest_framework',
+    'ml',
 ]
 
 MIDDLEWARE = [
